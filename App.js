a77ba572d4fd1a697096d74c1913f1c4e8c8f8bb
DanielorRyker <hoangraper2017@gmail.com>
2024-09-29 13:34:29 +0700

    Create Screen_01 UI and Add navigation to Sign Up and Login buttons

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import SignUpScreen from './SignUpScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer 
+      initialRouteName="Home"
+     
+    >
+      <Stack.Navigator >
+        <Stack.Screen name="Home" component={HomeScreen}  options={{headerShown: false}}/>
+        <Stack.Screen name="SignUp" component={SignUpScreen}  options={{headerShown: false}}/>
+        {/* <Stack.Screen name="Login" component={SigninScreen} /> 
+        <Stack.Screen name="Product" component={DashboardScreen} /> */}
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
 
